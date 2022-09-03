e1fbfe1bc383bf03908e3bbe7c2c6494299af4fc
molto <bodymgahed@yahoo.com>
2022-09-03 12:46:11 +0200

    main function

--- app.js
+++ app.js
@@ -1,110 +1,63 @@
-const form = document.getElementById("form");
-const serialNum = document.querySelector(".serial-num");
+const form = document.querySelector("form");
 const holderName = document.querySelector(".card-holder-name");
+const serialNum = document.querySelector(".serial-num");
 const expDate = document.querySelector(".card-exp-date");
 const pin = document.querySelector(".pin");
+const error = document.querySelector(".error");
+const confirm = document.querySelector(".confirm");
 //inputs
 const allInput = document.querySelectorAll("input");
 const nameInput = document.querySelector("#name");
 const numberInput = document.querySelector("#number");
+const cardInputs = document.querySelector(".inputs");
 const exMonth = document.querySelector("#ex-month");
 const exYear = document.querySelector("#ex-year");
 const cvc = document.querySelector("#cvc");
 // display
 const inputView = document.querySelector(".data-sec");
 const submittedView = document.querySelector(".submitted");
-window.onload = submittedView.style.display = "none";
 
+//
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  validInputs();
-  if (allInput.classList.contains("ok")) {
+  const errorMessage1 = document.querySelector(".error1");
+  const errorMessage2 = document.querySelector(".error2");
+  const errorMessage3 = document.querySelector(".error3");
+  if (nameInput.value.trim() == "" && nameInput.value.trim() !== String) {
+    nameInput.style.border = "2px solid red";
+    errorMessage1.style.display = "";
+    errorMessage1.textContent = "please enter your name";
+    console.log(nameInput.value);
+  } else if (
+    numberInput.value.trim() == "" &&
+    numberInput.value.trim() !== Number
+  ) {
+    numberInput.style.border = "2px solid red";
+    errorMessage2.style.display = "";
+    errorMessage2.textContent = "please enter numbers only";
+  } else if (exMonth.value.trim() == "" && exMonth.value.trim() !== Number) {
+    exMonth.style.border = "2px solid red";
+    errorMessage3.textContent = "please enter numbers only";
+    errorMessage3.style.display = "";
+  } else if (exYear.value.trim() == "" && exYear.value.trim() !== Number) {
+    exYear.style.border = "2px solid red";
+    errorMessage3.textContent = "please enter numbers only";
+    errorMessage3.style.display = "";
+  } else if (cvc.value.trim() == "" && cvc.value.trim() !== Number) {
+    cvc.style.border = "2px solid red";
+    errorMessage3.textContent = "please enter numbers only";
+    errorMessage3.style.display = "";
+  } else {
+    nameInput.style.border = "2px solid green";
+    numberInput.style.border = "2px solid green ";
+    cardInputs.style.border = "2px solid green ";
+    exMonth.style.border = "2px solid green ";
+    exYear.style.border = "2px solid green ";
     inputView.style.display = "none";
     submittedView.style.display = "";
-  } else {
-    return;
+    holderName.textContent = nameInput.value;
+    serialNum.textContent = numberInput.value;
+    expDate.textContent = `${exMonth.value} / ${exYear.value}`;
+    pin.textContent = cvc.value;
   }
 });
-
-function isValid(name) {
-  const regex = /^[a-zA-Z ]{2,20}$/;
-  return regex.test(String(name).toLocaleLowerCase());
-}
-function isNumber(number) {
-  const regex = /^[0-9]*\d$/;
-  const long = 16;
-  return regex.test(number);
-}
-function isMonth(month) {
-  return month != 00 && month.length <= 2 && month <= 12;
-}
-
-function isYear(year) {
-  return year != 00 && year.length <= 2 && year <= 30;
-}
-
-function isCvc(cvc) {
-  return cvc != 00 && cvc.length == 3 && cvc < 999;
-}
-
-const validInputs = () => {
-  let nameValue = nameInput.value.trim();
-  let numberValue = numberInput.value.trim();
-  let exMonthValue = exMonth.value.trim();
-  let exYearValue = exYear.value.trim();
-  let cvcValue = cvc.value.trim();
-
-  if (
-    nameValue == "" ||
-    nameValue == undefined ||
-    nameValue !== String ||
-    nameValue !== isValid(nameValue)
-  ) {
-    nameInput.style.border = "2px solid red";
-    nameInput.style.animation = "shakeme 0.5s ease-in-out 0s 2";
-  }
-  if (isValid(nameValue)) {
-    nameInput.style.border = "2px solid green";
-    holderName.textContent = nameValue;
-    nameInput.classList.add("ok");
-  }
-
-  if (numberValue == "" || numberValue !== isNumber(nameValue)) {
-    numberInput.style.border = "2px solid red";
-    numberInput.style.animation = "shakeme 0.5s ease-in-out 0s 2";
-  }
-  if (isNumber(numberValue)) {
-    numberInput.style.border = "2px solid green";
-    serialNum.textContent = numberValue;
-    serialNum.classList.add("ok");
-  }
-
-  if (exMonthValue == "" || exMonthValue !== isMonth(exMonthValue)) {
-    exMonth.style.border = "2px solid red";
-    exMonth.style.animation = "shakeme 0.5s ease-in-out 0s 2";
-  }
-  if (isMonth(exMonthValue)) {
-    exMonth.style.border = "2px solid green";
-    expDate.textContent = `${exMonthValue}/${exYearValue}`;
-    exMonth.classList.add("ok");
-  }
-
-  if (exYearValue == "" || exYearValue !== isYear(exYearValue)) {
-    exYear.style.border = "2px solid red";
-    exYear.style.animation = "shakeme 0.5s ease-in-out 0s 2";
-  }
-  if (isYear(exYearValue)) {
-    exYear.style.border = "2px solid green";
-    exYear.classList.add("ok");
-  }
-
-  if (cvcValue == "" || cvcValue !== isCvc(cvcValue)) {
-    cvc.style.border = "2px solid red";
-    cvc.style.animation = "shakeme 0.5s ease-in-out 0s 2";
-  }
-  if (isCvc(cvcValue)) {
-    cvc.style.border = "2px solid green";
-    pin.textContent = cvcValue;
-    cvc.classList.add("ok");
-  }
-};
